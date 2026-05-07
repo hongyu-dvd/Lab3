0fae7c22e382cc85c10f8eb2f60770eed1bcc31a
Zenghongyu <hongyu08.25@ichat.sp.edu.sg>
2026-05-07 14:51:51 +0800

    lab3

--- Lab3.py
+++ Lab3.py
@@ -12,11 +12,22 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
-    if n < 10:
-        # Traverse through all array elements
-        for i in range(n - 1):
-            # range(n) also work but outer loop will
-            # repeat one time more than needed.
+    if n == 0:
+        return 0
+
+    if not all(type(value) is int for value in arr_result):
+        return 2
+    
+    if n >= 10:
+        return 1
+
+    if sorting_order not in (SORT_ASCENDING, SORT_DESCENDING):
+        return []
+
+    # Traverse through all array elements
+    for i in range(n - 1):
+        # range(n) also works but outer loop will
+        # repeat one time more than needed.
 
             # Last i elements are already in place
             for j in range(0, n - i - 1):
@@ -29,12 +40,6 @@ def bubble_sort(arr, sorting_order):
                 elif sorting_order == SORT_DESCENDING:
                     if arr_result[j] < arr_result[j + 1]:
                         arr_result[j], arr_result[j + 1] = arr_result[j + 1], arr_result[j]
-
-                else:
-                    # Return an empty array
-                    arr_result = []
-    else:
-        arr_result = -1
 
     return arr_result
 
